c9c795eca6513d5561b00be593ba6d81436de4ca
shweta-rc13 <shwetachaurasiya1313@gmail.com>
2024-04-02 23:48:37 +0530

    TextEase

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 
 import { useState } from 'react';
 import './App.css';
-// import About from './Components/About';
-// import Navbar from './Components/Navbar';
+import About from './Components/About';
+import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 
 
 function App() {
-  // const [mode, setMode] = useState("light");
+  const [mode, setMode] = useState("light");
 
   return (
   <>
- {/* <Navbar title="TextUtils" mode={mode}/> */}
- {/* <Navbar/> */}
+ {/* <Navbar title="TextUtils" mode={mode}/>  */}
+  <Navbar/>
 <div className="container my-3">
 <TextForm heading="Enter the text to Analyze  "/>
-{/* <About/> */}
+ {/* <About/>  */}
 
 </div>
   </>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -26,10 +26,16 @@ const Navbar = (props) => {
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <button className="btn btn-outline-success" type="submit">Search</button>
       </form>
+      <div class="form-check form-switch">
+  <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+  <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
+</div>
+
     </div>
   </div>
 </nav>
     </div>
+
   )
 }
 
